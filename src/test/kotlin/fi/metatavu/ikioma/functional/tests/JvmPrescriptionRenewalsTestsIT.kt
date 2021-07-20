7dfa5b927b2cddd42f8c39a26c6183b7282cacdb
Antti Leppä <antti.leppa@metatavu.fi>
2021-07-20 08:55:54 +0300

    Added tests for native builds

--- src/test/kotlin/fi/metatavu/ikioma/functional/tests/JvmPrescriptionRenewalsTestsIT.kt
+++ src/test/kotlin/fi/metatavu/ikioma/functional/tests/JvmPrescriptionRenewalsTestsIT.kt
@@ -1,6 +1,7 @@
 package fi.metatavu.ikioma.functional.tests
 
 import io.quarkus.mailer.MockMailbox
+import io.quarkus.test.junit.DisabledOnNativeImage
 import io.quarkus.test.junit.QuarkusTest
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
@@ -10,6 +11,7 @@ import javax.inject.Inject
  * JVM tests for prescription renewals API
  */
 @QuarkusTest
+@DisabledOnNativeImage
 class JvmPrescriptionRenewalsTestsIT: BasePrescriptionRenewalsTestsIT() {
 
     @Inject
--- src/test/kotlin/fi/metatavu/ikioma/functional/tests/NativePrescriptionRenewalsTestsIT.kt
+++ src/test/kotlin/fi/metatavu/ikioma/functional/tests/NativePrescriptionRenewalsTestsIT.kt
@@ -1,10 +1,12 @@
 package fi.metatavu.ikioma.functional.tests
 
 import io.quarkus.test.junit.NativeImageTest
+import io.quarkus.test.junit.QuarkusTest
 
 /**
  * Native tests for prescription renewals API
  */
+@QuarkusTest
 @NativeImageTest
 class NativePrescriptionRenewalsTestsIT: BasePrescriptionRenewalsTestsIT() {
 
